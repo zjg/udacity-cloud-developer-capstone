03bb966d842439e0e0eee7d57ca3454c61efe4c3
Doug Rosvick <drosvick@gmail.com>
2021-10-31 12:23:04 -0600

    Implement property editing in frontend

--- client/src/components/EditTodo.tsx
+++ client/src/components/EditTodo.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
 import { Form, Button } from 'semantic-ui-react'
 import Auth from '../auth/Auth'
-import { getUploadUrl, uploadFile } from '../api/todos-api'
+import { getUploadUrl, uploadFile, getSingleTodo, patchTodo } from '../api/todos-api'
+import { useParams } from 'react-router'
 
 enum UploadState {
   NoUpload,
@@ -21,6 +22,12 @@ interface EditTodoProps {
 interface EditTodoState {
   file: any
   uploadState: UploadState
+  name: string
+  dueDate: string
+  public: boolean
+  done: boolean
+  loading: boolean
+  updatingProps: boolean
 }
 
 export class EditTodo extends React.PureComponent<
@@ -29,7 +36,28 @@ export class EditTodo extends React.PureComponent<
 > {
   state: EditTodoState = {
     file: undefined,
-    uploadState: UploadState.NoUpload
+    uploadState: UploadState.NoUpload,
+    name: '',
+    dueDate: '',
+    public: false,
+    done: false,
+    loading: true,
+    updatingProps: false,
+  }
+
+  async componentDidMount() {
+    try {
+      const todo = await getSingleTodo(this.props.auth.getIdToken(), this.props.match.params.todoId)
+      this.setState({
+        loading: false,
+        name: todo.name,
+        dueDate: todo.dueDate,
+        done: todo.done,
+        public: todo.public,
+      })
+    } catch (e) {
+      alert(`Failed to fetch todos: ${e}`)
+    }
   }
 
   handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -70,6 +98,42 @@ export class EditTodo extends React.PureComponent<
     })
   }
 
+  updateProps = async (event: React.SyntheticEvent) => {
+    event.preventDefault()
+
+    try {
+      if (!this.state.name.match("[^ ]+")) {
+        alert('Name cannot be blank')
+        return
+      }
+
+      this.setState({updatingProps: true})
+      await patchTodo(this.props.auth.getIdToken(), this.props.match.params.todoId, {
+        name: this.state.name,
+        dueDate: this.state.dueDate,
+        done: this.state.done,
+        public: this.state.public,
+      })
+    } catch (e) {
+      alert(`Could not update props: ${e}`)
+    } finally {
+      this.setState({updatingProps: false})
+    }
+  }
+
+  nameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({name: event.target.value})
+  }
+  dueDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({dueDate: event.target.value})
+  }
+  publicChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({public: event.target.checked})
+  }
+  doneChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({done: event.target.checked})
+  }
+
   render() {
     return (
       <div>
@@ -88,6 +152,26 @@ export class EditTodo extends React.PureComponent<
 
           {this.renderButton()}
         </Form>
+        <h1>Modify Properties</h1>
+        <Form onSubmit={this.updateProps}>
+          <Form.Field>
+            <label>Name</label>
+            <input type="text" name="name" value={this.state.name} onChange={this.nameChange}/>
+          </Form.Field>
+          <Form.Field>
+            <label>Due Date</label>
+            <input type="date" name="dueDate" value={this.state.dueDate} onChange={this.dueDateChange}/>
+          </Form.Field>
+          <Form.Field>
+            <label>Public</label>
+            <input type="checkbox" name="public" checked={this.state.public} onChange={this.publicChange}/>
+          </Form.Field>
+          <Form.Field>
+            <label>Done</label>
+            <input type="checkbox" name="done" checked={this.state.done} onChange={this.doneChange}/>
+          </Form.Field>
+          {this.renderPropsButton()}
+        </Form>
       </div>
     )
   }
@@ -103,6 +187,19 @@ export class EditTodo extends React.PureComponent<
           type="submit"
         >
           Upload
+        </Button>
+      </div>
+    )
+  }
+  renderPropsButton() {
+
+    return (
+      <div>
+        <Button
+          loading={this.state.updatingProps}
+          type="submit"
+        >
+          Update TODO
         </Button>
       </div>
     )
