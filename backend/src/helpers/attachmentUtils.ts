ced78760c81a2e33ef127b127c0cc1b5441f2330
Doug Rosvick <drosvick@gmail.com>
2021-11-01 01:45:10 -0600

    fix s3 client not working offline

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -1,13 +1,9 @@
 import * as AWS from 'aws-sdk'
 import * as AWSXRay from 'aws-xray-sdk'
 
-const XAWS = AWSXRay.captureAWS(AWS)
-
 export class AttachmentUtils {
     constructor(
-        private readonly s3Client = new XAWS.S3({
-            signatureVersion: 'v4'
-        }),
+        private readonly s3Client = createS3Client(),
         private readonly bucketName: string = process.env.ATTACHMENT_S3_BUCKET,
         private readonly expirationTime: number = +process.env.SIGNED_URL_EXPIRATION,
     ) {}
@@ -23,4 +19,16 @@ export class AttachmentUtils {
         )
     }
 
+}
+
+function createS3Client() {
+    const params = {
+        signatureVersion: 'v4'
+    }
+    if (process.env._X_AMZN_TRACE_ID) {
+        const XAWS = AWSXRay.captureAWS(AWS)
+        return new XAWS.S3(params)
+    } else {
+        return new AWS.S3(params);
+    }
 }
